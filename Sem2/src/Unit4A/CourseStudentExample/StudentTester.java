793d9430706965780a32b622b5fa9eebafc174c7
Vinita-G <cabbageheep@gmail.com>
2026-01-09 14:09:53 -0600

    Had issues commiting code this is my second try

--- Sem2/src/Unit4A/CourseStudentExample/StudentTester.java
+++ Sem2/src/Unit4A/CourseStudentExample/StudentTester.java
@@ -2,6 +2,7 @@ package Unit4A.CourseStudentExample;
 
 public class StudentTester {
     public static void main(String[] args){
+        System.out.println("There is a 40 character cap on the subject names");
         Course pe = new Course("Vlaming", "Basketball PE", "A", 1);
         Course math = new Course("Higgins", "Trig Calc A", "B+", 2);
         Course sci = new Course("O'Dette", "Physics 1", "A-", 3);
@@ -10,7 +11,6 @@ public class StudentTester {
         Course psych = new Course("Ganas", "AP Psych", "B+", 6);
         Course studyHall = new Course("Study Hall", 7);
         Course spanish = new Course("Lernor", "Spanish 3", "A-", 8);
-
 
         Course[] list = {pe, math, sci, lunch, lit, psych, studyHall, spanish};
         Student a = new Student("Molly Jones", list);
